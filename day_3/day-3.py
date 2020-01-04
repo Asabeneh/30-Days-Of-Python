e3379fff3f3b742562b1eb824940ca23bd302d3a
Christian Clauss <cclauss@me.com>
2020-01-04 20:21:17 +0100

    Use ==/!= to compare str, bytes, and int literals
    Identity is not the same thing as equality in Python so use ==/!= to compare str, bytes, and int literals. In Python >= 3.8, these instances will raise __SyntaxWarnings__ so it is best to fix them now. https://docs.python.org/3.8/whatsnew/3.8.html#porting-to-python-3-8

--- day_3/day-3.py
+++ day_3/day-3.py
@@ -102,13 +102,13 @@ print('False == False:', False == False)
 print('True and True: ', True and True)
 print('True or False:', True or False)
 # Another way comparison
-print('1 is 1', 1 is 1)                   # True - because the data values are the same
-print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+print('1 == 1', 1 == 1)                   # True - because the data values are the same
+print('1 != 2', 1 != 2)                   # True - because 1 is not 2
 print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
 print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
 print('coding' in 'coding for all') # True - because coding for all has the word coding
 print('a in an:', 'a' in 'an')      # True
-print('4 is 2 ** 2:', 4 is 2 **2)   # True
+print('4 == 2 ** 2:', 4 == 2 **2)   # True
 
 print(3 > 2 and 4 > 3) # True - because both statements are true
 print(3 > 2 and 4 < 3) # False - because the second statement is false
