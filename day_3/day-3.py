e39faa87831579455003dc6a6b9b93fb9569c2c3
Christian Clauss <cclauss@me.com>
2020-01-04 20:24:06 +0100

    Undefined name: area_of_width -> area_of_rectangle

--- day_3/day-3.py
+++ day_3/day-3.py
@@ -72,7 +72,7 @@ print('Area of a circle:', area_of_circle)
 length = 10
 width = 20
 area_of_rectangle = length * width
-print('Area of rectangle:', area_of_width)
+print('Area of rectangle:', area_of_rectangle)
 
 # Calculating a weight of an object
 mass = 75
