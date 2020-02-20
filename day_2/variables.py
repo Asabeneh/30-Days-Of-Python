680124c626b363f6ad3cc4bebbb8eb519893e773
KellyHolleran <kholleran@esri.com>
2020-02-20 06:59:38 -0800

    Completed some of Day 2 exercises

---
+++ day_2/variables.py
@@ -0,0 +1,23 @@
+#Day 2: 30 Days of python programming
+first_name = 'Kelly'
+last_name = 'Holleran'
+full_name = 'Kelly Ann Holleran'
+Country = 'United States'
+City = 'Loma Linda'
+Age = 28
+Year = 1991
+is_married = False
+is_true = True
+is_light_on = 'Yes'
+
+first_name, last_name, country, age, is_married = 'Kelly', 'Holleran', 'United States', 28, False
+
+print(type('Kelly'))
+print(type('first_name'))
+print(type('Age'))
+print(type(28))
+print(type('is_married'))
+print(type('true'))
+print(type('Kelly'))
+print(type(first_name))
+
