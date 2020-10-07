2e3d295c8398494d10635e774b6590215eeafa9c
Tej Pratap Yadav <60545022+tyadav4268@users.noreply.github.com>
2020-10-07 06:32:27 +0530

    Update variables.py
    Added some descriptions regarding the codes.

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,13 +1,22 @@
 
 # Variables in Python
+# There is no need to state the data type, 
+# variables can be declared with name and data in them can be stored directly.
+# Pyrhon automatically recognises the data type.
 
+# String literals in python are surrounded by either single quotation marks or double quotation marks.
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
+acountry = "Finland"         
+city = "Helsinki"
+
 age = 250
 is_married = True
+
+# A list can be used to store multiple values
 skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+
+# A dictionary can be used to store key-value pairs.
 person_info = {
     'firstname':'Asabeneh', 
     'lastname':'Yetayeh', 
@@ -16,7 +25,6 @@ person_info = {
     }
 
 # Printing the values stored in the variables
-
 print('First name:', first_name)
 print('First name length:', len(first_name))
 print('Last name: ', last_name)
@@ -29,7 +37,6 @@ print('Skills: ', skills)
 print('Person information: ', person_info)
 
 # Declaring multiple variables in one line
-
 first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
 
 print(first_name, last_name, country, age, is_married)
