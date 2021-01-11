44b75257ec9eb4724f7c2baadb91e5dae121135b
starlordsaraf <mainakisaraf@gmail.com>
2021-01-11 20:18:11 +0530

    fixed string len after concatenating space

--- 04_Day_Strings/day_4.py
+++ 04_Day_Strings/day_4.py
@@ -23,14 +23,14 @@ print(multiline_string)
 # String Concatenation
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
-space = ' '
-full_name = first_name  +  space + last_name
+space_between = ' '
+full_name = first_name  +  space_between + last_name
 print(full_name) # Asabeneh Yetayeh
 # Checking length of a string using len() builtin function
 print(len(first_name))  # 8
 print(len(last_name))   # 7
 print(len(first_name) > len(last_name)) # True
-print(len(full_name)) # 15
+print(len(full_name)) # 16
 
 #### Unpacking characters 
 language = 'Python'
