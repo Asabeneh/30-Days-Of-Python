a2572ea20bd694023747ef90cbb7227e909ce82a
Chandd224 <76743111+Chandd224@users.noreply.github.com>
2021-01-11 19:52:39 -0500

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,45 @@
+# Day 2: 30 Days of python programming
+first_name = 'Chandler'
+last_name = 'Frink'
+fullname = 'Chandler Frink'
+country = 'US'
+city = 'Charleston'
+age = 25
+year = '2021'
+is_married = False
+is_true = False
+is_light_on = False
+first_name, last_name, country, age = 'Chandler', 'Frink', 'US', 25
+
+print(type(first_name))
+print(type(last_name))
+print(type(fullname))
+print(type(country)
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(first_name) == len(last_name))
+
+num_one = 5
+num_two = 4
+variable_total = (num_one) + (num_two)
+variable_diff = (num_one) - (num_two)
+variable_product = (num_two) * (num_one)
+variable_division = (num_one)/(num_two)
+variable_remainder = (num_one) % (num_two)
+variable_exp = num_one**num_two
+variable_floor_division = num_one // num_two
+
+r = 30
+area_of_circle = 3.14r**2
+circum_of_circle = (2)(3.14r)
+
+first_name = input('First name: ')
+last_name = input('Last name: ')
+country = input('Country: ')
+age = input('age: ')
