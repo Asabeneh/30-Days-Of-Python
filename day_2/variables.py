b5962c8c2244c824d8a513c5a2d923512957df7a
Chandd224 <76743111+Chandd224@users.noreply.github.com>
2021-01-12 19:48:19 -0500

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -15,23 +15,22 @@ print(type(first_name))
 print(type(last_name))
 print(type(fullname))
 print(type(country)
-print(type(city))
-print(type(age))
-print(type(year))
-print(type(is_married))
-print(type(is_true))
-print(type(is_light_on))
+print(type(city)
+print(type(age)
+print(type(year)
+print(type(is_married)
+print(type(is_true)
+print(type(is_light_on)
 
 print(len(first_name))
-print(len(first_name) == len(last_name))
 
 num_one = 5
 num_two = 4
-variable_total = (num_one) + (num_two)
-variable_diff = int(num_one) - int(num_two)
-variable_product = int(num_two) * int(num_one)
-variable_division = int(num_one)/int(num_two)
-variable_remainder = int(num_one) % int(num_two)
+variable_total = num_one + num_two
+variable_diff = num_one - num_two
+variable_product = num_two * num_one
+variable_division = num_one / num_two
+variable_remainder = num_one % num_two
 variable_exp = num_one**num_two
 variable_floor_division = num_one // num_two
 
