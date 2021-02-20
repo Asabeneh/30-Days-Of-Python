1f0200f12d4a92073a7c60bbedb2acb204cd5d5b
dorindabassey <53014273+dorindabassey@users.noreply.github.com>
2021-02-20 11:23:26 +0100

    Update day_5.py
    changed value of last _index which caused IndexError: list assignment index out of range

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -58,7 +58,7 @@ fruits[0] = 'Avocado'
 print(fruits)       #  ['avocado', 'orange', 'mango', 'lemon']
 fruits[1] = 'apple'
 print(fruits)       #  ['avocado', 'apple', 'mango', 'lemon']
-last_index = len(fruits)
+last_index = len(fruits) - 1
 fruits[last_index] = 'lime'
 print(fruits)        #  ['avocado', 'apple', 'mango', 'lime']
 
