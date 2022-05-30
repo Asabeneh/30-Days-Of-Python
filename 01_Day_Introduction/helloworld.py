7ed1cdc8ffbf859c19536f7cbb87a2968a057fd8
achkr <alexandrechekir@hotmail.fr>
2022-05-30 18:27:46 +0200

    Comment on file 1

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,3 +19,6 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+# this is a comment
+a = [1:10]
