6b7cab2e00a2a0fb5a33816cc090442b5b2419cd
Irina Statnaia <irina.statnaia@gmail.com>
2022-07-19 17:13:51 +0300

    string update

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -6,8 +6,8 @@ print(3 - 1)   # subtraction(-)
 print(2 * 3)   # multiplication(*)
 print(3 / 2)   # division(/)
 print(3 ** 2)  # exponential(**)
-print(9 % 2)   # modulus(%)
-print(9 // 2)  # Floor division operator(//)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
 
