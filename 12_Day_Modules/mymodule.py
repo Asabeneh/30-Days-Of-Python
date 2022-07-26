4bc54662f8dfbf54b20393ca8f7e2d531d864f68
Piyush Srivastava <67270567+piyush-sri@users.noreply.github.com>
2022-07-26 23:05:22 +0530

    added comments for clear understanding

--- 12_Day_Modules/mymodule.py
+++ 12_Day_Modules/mymodule.py
@@ -1,6 +1,8 @@
+#Function definition   Parameter1, Parameter2
+#function returning a value also known as fruitful Function
 def generate_full_name(firstname, lastname):
       space = ' '
-      fullname = firstname + space + lastname
+      fullname = firstname + space + lastname #concatenation
       return fullname
 
 def sum_two_nums (num1, num2):
@@ -11,6 +13,7 @@ person = {
     "age": 250,
     "country": "Finland",
     "city":'Helsinki'
-}
+}#person is a Dictionary containing Key And Value Pairs and each pairs are seperated by comma.
+
 
 
