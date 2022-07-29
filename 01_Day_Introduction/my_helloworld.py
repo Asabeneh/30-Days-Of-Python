e348cff661426c292e554ef3a028d3fdc914de47
Iván González <cafeteru.dev@gmail.com>
2022-07-29 19:54:52 +0200

    Exercise 01

---
+++ 01_Day_Introduction/my_helloworld.py
@@ -0,0 +1,28 @@
+from ensurepip import version
+
+# Day 1 - 30DaysOfPython Challenge
+# Exercise: Level 1
+print("Exercise: Level 1")
+print("- Open the python interactive shell and do the following operations. The operands are 3 and 4.")
+print("\taddition: 3 + 4 =", 3 + 4)
+print("\tsubtraction: 3 - 4 =", 3 - 4)
+print("\tmultiplication: 3 * 4 =", 3 * 4)
+print("\tdivision: 3 / 4 =", 3 / 4)
+print("\texponential: 3 ** 4 =", 3 ** 4)
+print("\tfloor division operator: 3 // 4 =", 3 // 4)
+print("- Write strings on the python interactive shell. The strings are the following:")
+print("\tIván")
+print("\tGonzález")
+print("\tSpain")
+print("\tI am enjoying 30 days of python")
+print("- Check the data types of the following data:")
+print("\t10: ", type(10))
+print("\t9.8: ", type(9.8))
+print("\t3.14: ", type(3.14))
+print("\t4 - 4j: ", type(4 - 4j))
+print("\t['Asabeneh', 'Python', 'Finland']: ", type(['Asabeneh', 'Python', 'Finland']))
+print("\tIván: ", type("Iván"))
+print("\tGonzález: ", type("tGonzález"))
+print("\tSpain: ", type("tSpain"))
+
+print("Exercise: Level 2")
