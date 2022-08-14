ed9f0559ea0c2e00668752efd63b592266cb480e
Noe Sola <ariadnnasc@gmail.com>
2022-08-14 22:25:48 +0200

    feat: exercises day 01 complete

---
+++ 01_Day_Introduction/day_01/helloworld.py
@@ -0,0 +1,73 @@
+# 1º Check the python version you are using
+# python --version : Python 3.8.13
+
+# Exercise: Level 2
+# Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. 
+# Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it. 
+# The operands are 3 and 4.
+
+# addition(+)
+print(3+4)
+# subtraction(-)
+print(4-3)
+# multiplication(*)
+print(3*4)
+# modulus(%)
+print(3%4)
+# division(/)
+print(3/4)
+# exponential(**)
+print(3**4)
+# floor division operator(//)
+print(3//4)
+# Write strings on the python interactive shell. The strings are the following:
+# Your name
+print('Noemi')
+# Your family name
+print('Sola')
+# Your country
+print('Spain')
+# I am enjoying 30 days of python
+print('I am enjoying 30 days of python')
+# Check the data types of the following data:
+# 10
+print(type(10))
+# <class 'int'>
+# 9.8
+print(type(9.8))
+# <class 'float'>
+# 3.14
+print(type(3.14))
+# <class 'float'>
+# 4 - 4j
+print(type( 4 - 4j))
+# <class 'complex'>
+# ['Asabeneh', 'Python', 'Finland']
+print(type (['Asabeneh', 'Python', 'Finland']))
+# <class 'list'>
+# Your name
+print(type('Noemi'))
+# <class 'str'>
+# Your family name
+print(type ('Sola'))
+# <class 'str'>
+# Your country
+print(type ('Spain'))
+# <class 'str'>
+
+# Exercise: Level 3
+# Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print(type(50))
+print(type(7.24))
+print(type( 2 - 14j))
+print(type(False))
+print(type('Baloo'))
+print(type(['Jesús', 'Red', 'Frozen']))
+print(type({9.8, 3.14, 2.7}))
+print(type((9.8, 3.14, 2.7)))
+print(type({'age':34}))
+
+
+
+# Find an Euclidian distance between (2, 3) and (10, 8)
+print("Euclidean distance = ", ((3 - 4) ** 2 + (9 - 14) ** 2) ** 0.5)
