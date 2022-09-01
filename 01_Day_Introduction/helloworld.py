9fd3df0073204b0887c99c19b8d78e79565bf880
Sara Bagherieh <93508577+s-bgh00@users.noreply.github.com>
2022-09-01 12:19:42 +0430

    Update helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,21 +1,37 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
-print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+print(3 + 4)   # addition(+)
+print(3 - 4)   # subtraction(-)
+print(3 * 4)   # multiplication(*)
+print(3 / 4)   # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 % 4)   # modulus(%)
+print(3 // 4)  # Floor division operator(//)
 
 # Checking data types
 
 print(type(10))                  # Int
+print(type(9.8))                 #Float
 print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(4 - 4j))              # Complex
+print(type['Asabeneh', 'Python', 'Finland'])          # List
+print(type("Sara"))               # String
+print(type("Bgh"))               #String
+print(type("Iran"))              #String
+      
+# Level 3
+print(type(13))  #Int
+print(type(13.13)) #Float
+print(type(13 + 13j)) #Complex
+print(type("Sara")) #String
+print(type(True))   #Bool
+print(type([1, 2, 3])) #List
+print(type({'first_name' : 'Sara', 'last_name' : 'Bgh', 'age' : 22})) #Dict
+print(type(("sara", "bgh"))) #tuple
+print(type({1, 2, 3})) #set
+      
+import math as mt
+euc_dis = mt.sqrt(((8 - 3) ** 2) + ((10 - 2) ** 2))
+print(euc_dis)
+      
