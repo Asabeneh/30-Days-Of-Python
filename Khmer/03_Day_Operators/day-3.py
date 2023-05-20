fec4494b8b0a60bbb0fa7b3764ecc4dfc8550838
Sivtheng <sivtheng25@gmail.com>
2023-05-20 09:54:51 +0700

    day 3 dot py

--- Khmer/03_Day_Operators/day-3.py
+++ Khmer/03_Day_Operators/day-3.py
@@ -1,4 +1,4 @@
-# ប្រតិបត្តិការ គណិតវិទ្យា ក្នុង Python
+# ប្រតិបត្តិការនព្វន្ធ ក្នុង Python
 # ចំនួនគត់
 
 print('Addition: ', 1 + 2)
@@ -20,12 +20,12 @@ print('Floating Number, gravity', 9.81)
 print('Complex number: ', 1 + 1j)
 print('Multiplying complex number: ',(1 + 1j) * (1-1j))
 
-# Declaring the variable at the top first
+# បង្កើតអថេរនៅខាងលើមុន
 
-a = 3 # a is a variable name and 3 is an integer data type
-b = 2 # b is a variable name and 3 is an integer data type
+a = 3 # a ជាឈ្មោះអថេរ និង 3 ជាចំនួនគត់
+b = 2 # b ជាឈ្មោះអថេរ និង 2 ជាចំនួនគត់
 
-# Arithmetic operations and assigning the result to a variable
+# ប្រតិបត្តិការនព្វន្ធ និង ការដាក់ចម្លើយនៅក្នុងអថេរ
 total = a + b
 diff = a - b
 product = a * b
@@ -34,8 +34,7 @@ remainder = a % b
 floor_division = a // b
 exponential = a ** b
 
-# I should have used sum instead of total but sum is a built-in function try to avoid overriding builtin functions
-print(total) # if you don't label your print with some string, you never know from where is  the result is coming
+print(total)
 print('a + b = ', total)
 print('a - b = ', diff)
 print('a * b = ', product)
@@ -44,49 +43,49 @@ print('a % b = ', remainder)
 print('a // b = ', floor_division)
 print('a ** b = ', exponential)
 
-# Declaring values and organizing them together
+# បង្កើតអថេរនិងតម្លៃ 2 ហើយរៀបវាជាមួយគ្នា
 num_one = 3
 num_two = 4
 
-# Arithmetic operations
+# ប្រតិបត្តិការនព្វន្ធ
 total = num_one + num_two
 diff = num_two - num_one
 product = num_one * num_two
 div = num_two / num_two
 remainder = num_two % num_one
 
-# Printing values with label
+# បង្ហាញចម្លើយជាមួយឈ្មោះរបស់វា
 print('total: ', total)
 print('difference: ', diff)
 print('product: ', product)
 print('division: ', div)
 print('remainder: ', remainder)
 
-
-# Calculating area of a circle
-radius = 10                                 # radius of a circle
-area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+# ការគណនាផ្ទៃក្រឡារង្វង់
+radius = 10                                 # កាំនៃរង្វង់
+area_of_circle = 3.14 * radius ** 2         # ** មានន័យថាស្វាយគុណ
 print('Area of a circle:', area_of_circle)
 
-# Calculating area of a rectangle
+# ការគណនាផ្ទៃក្រឡានៃចតុកោណកែង
 length = 10
 width = 20
 area_of_rectangle = length * width
 print('Area of rectangle:', area_of_rectangle)
 
-# Calculating a weight of an object
+# ការគណនាទម្ងន់នៃវត្ថុមួយ។
 mass = 75
 gravity = 9.81
 weight = mass * gravity
 print(weight, 'N')
 
-print(3 > 2)     # True, because 3 is greater than 2
-print(3 >= 2)    # True, because 3 is greater than 2
-print(3 < 2)     # False,  because 3 is greater than 2
-print(2 < 3)     # True, because 2 is less than 3
-print(2 <= 3)    # True, because 2 is less than 3
-print(3 == 2)    # False, because 3 is not equal to 2
-print(3 != 2)    # True, because 3 is not equal to 2
+# True = ពិត , False = មិនពិត
+print(3 > 2)     # True, ដោយសារតែ 3 គឺធំជាង 2
+print(3 >= 2)    # True, ដោយសារតែ 3 គឺធំជាង 2
+print(3 < 2)     # False,  ដោយសារតែ 3 គឺធំជាង 2
+print(2 < 3)     # True, ដោយសារតែ 2 គឺតិចជាង 3
+print(2 <= 3)    # True, ដោយសារតែ 2 iគឺតិចជាង 3
+print(3 == 2)    # False, ដោយសារតែ 3 មិនស្មើនឹង 2
+print(3 != 2)    # True, ដោយសារតែ 3 មិនស្មើនឹង 2
 print(len('mango') == len('avocado'))  # False
 print(len('mango') != len('avocado'))  # True
 print(len('mango') < len('avocado'))   # True
@@ -95,30 +94,30 @@ print(len('milk') == len('meat'))      # True
 print(len('tomato') == len('potato'))  # True
 print(len('python') > len('dragon'))   # False
 
-# Boolean comparison
+# ការប្រៀបធៀបជាមួយសញ្ញា​ប្រមាណ
 print('True == True: ', True == True)
 print('True == False: ', True == False)
 print('False == False:', False == False)
 print('True and True: ', True and True)
 print('True or False:', True or False)
 
-# Another way comparison 
-print('1 is 1', 1 is 1)                   # True - because the data values are the same
-print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
-print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
-print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
-print('coding' in 'coding for all') # True - because coding for all has the word coding
+# វិធីមួយទៀត
+print('1 is 1', 1 is 1)                   # True - ដោយសារតែ តម្លៃដូចគ្នា
+print('1 is not 2', 1 is not 2)           # True - ដោយសារតែ 1 មិនមែន 2
+print('A in Asabeneh', 'A' in 'Asabeneh') # True - A មាននៅក្នុងក្រុមអក្សរ
+print('B in Asabeneh', 'B' in 'Asabeneh') # False - គ្មានអក្សរ B
+print('coding' in 'coding for all') # True - ដោយសារតែ "coding for all" មានពាក្យ "coding"
 print('a in an:', 'a' in 'an')      # True
 print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
 
-print(3 > 2 and 4 > 3) # True - because both statements are true
-print(3 > 2 and 4 < 3) # False - because the second statement is false
-print(3 < 2 and 4 < 3) # False - because both statements are false
-print(3 > 2 or 4 > 3)  # True - because both statements are true
-print(3 > 2 or 4 < 3)  # True - because one of the statement is true
-print(3 < 2 or 4 < 3)  # False - because both statements are false
-print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
-print(not True)      # False - Negation, the not operator turns true to false
+print(3 > 2 and 4 > 3) # True - ដោយសារតែ សេចក្តីទាំងពីរគឺពិត
+print(3 > 2 and 4 < 3) # False - ដោយសារតែ សេចក្តីទី 2 មិនពិត
+print(3 < 2 and 4 < 3) # False - ដោយសារតែ សេចក្តីទាំងពីរគឺមិនពិត
+print(3 > 2 or 4 > 3)  # True - ដោយសារតែ សេចក្តីទាំងពីរគឺពិត
+print(3 > 2 or 4 < 3)  # True - ដោយសារតែ សេចក្តីមួយគឺពិត
+print(3 < 2 or 4 < 3)  # False - ដោយសារតែ សេចក្តីទាំងពីរគឺមិនពិត
+print(not 3 > 2)     # False - ដោយសារតែ 3 > 2 គឺពិត, ហើយ not True បាន False
+print(not True)      # False - ការបដិសេធ, ប្រតិបត្តិ not ទទួលបាន true ទៅ false
 print(not False)     # True
 print(not not True)  # True
 print(not not False) # False
