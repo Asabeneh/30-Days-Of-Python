78177983e390a6f78232e2fde37faaae676aff59
Семен <130279205+punchalaken@users.noreply.github.com>
2023-06-16 00:14:55 +0300

    Update day_4.py
    Translated_code_sreings

--- 04_Day_Strings/day_4.py
+++ 04_Day_Strings/day_4.py
@@ -1,48 +1,49 @@
 
-# Single line comment
-letter = 'P'                # A string could be a single character or a bunch of texts
+# Одиночная строка
+letter = 'P'                # Строка может быть как одним символом, так и набором из символов (текстом)
 print(letter)               # P
 print(len(letter))          # 1
-greeting = 'Hello, World!'  # String could be  a single or double quote,"Hello, World!"
+greeting = 'Hello, World!'  # Строку можно создать используя одинарные или двойные кавычки,"Hello, World!"
 print(greeting)             # Hello, World!
 print(len(greeting))        # 13
-sentence = "I hope you are enjoying 30 days of python challenge"
+sentence = "Надеюсь, Вам нравится 30 дней испытаний Python."
 print(sentence)
 
-# Multiline String
-multiline_string = '''I am a teacher and enjoy teaching.
-I didn't find anything as rewarding as empowering people.
-That is why I created 30 days of python.'''
-print(multiline_string)
-# Another way of doing the same thing
-multiline_string = """I am a teacher and enjoy teaching.
-I didn't find anything as rewarding as empowering people.
-That is why I created 30 days of python."""
+# Многострочная строка
+multiline_string = '''Я - учитель и наслаждаюсь преподаванием.
+Я не нашел ничего такого, что было бы столь же благотворным, как наделение людей знаниями.
+Вот почему я создал 30 дней Python.'''
 print(multiline_string)
 
-# String Concatenation
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
+# Another way of doing the same thing
+multiline_string = """Я - учитель и наслаждаюсь преподаванием.
+Я не нашел ничего такого, что было бы столь же благотворным, как наделение людей знаниями.
+Вот почему я создал 30 дней Python."""
+print(multiline_string)
+
+# Конкатенация строк
+first_name = 'Питер'
+last_name = 'Паркер'
 space = ' '
 full_name = first_name  +  space + last_name
-print(full_name) # Asabeneh Yetayeh
-# Checking length of a string using len() builtin function
-print(len(first_name))  # 8
-print(len(last_name))   # 7
-print(len(first_name) > len(last_name)) # True
-print(len(full_name)) # 15
+print(full_name) # Питер Паркер
+# Проверка длинны строки с помощью встроенной функции len()
+print(len(first_name))  # 5
+print(len(last_name))   # 6
+print(len(last_name) > len(first_name)) # True
+print(len(full_name)) # 12
 
-#### Unpacking characters 
+#### Распаковка строк
 language = 'Python'
-a,b,c,d,e,f = language # unpacking sequence characters into variables
+a,b,c,d,e,f = language # распаковка строки в последовательность переменные
 print(a) # P
 print(b) # y
-print(c) # t 
+print(c) # t
 print(d) # h
 print(e) # o
 print(f) # n
 
-# Accessing characters in strings by index
+# Доступ к символам в строках по индексу
 language = 'Python'
 first_letter = language[0]
 print(first_letter) # P
@@ -52,197 +53,224 @@ last_index = len(language) - 1
 last_letter = language[last_index]
 print(last_letter) # n
 
-# If we want to start from right end we can use negative indexing. -1 is the last index
+# Если мы хотим начать с конца строки, мы можем использовать отрицательный индекс. -1 обозначает индекс последнего символа, -2 индекс второго символа с конца и т.д.
 language = 'Python'
 last_letter = language[-1]
 print(last_letter) # n
 second_last = language[-2]
 print(second_last) # o
 
-# Slicing
+# Срезы
 
 language = 'Python'
-first_three = language[0:3] # starts at zero index and up to 3 but not include 3
+first_three = language[0:3] # начинаем с нулевого индекса включительно и идем до 3 невключительно.
+print(first_three) #Pyt
 last_three = language[3:6]
 print(last_three) # hon
-# Another way
+# Другой способ
 last_three = language[-3:]
 print(last_three)   # hon
 last_three = language[3:]
 print(last_three)   # hon
 
-# Skipping character while splitting Python strings
+# Пропуск символов в срезах
 language = 'Python'
-pto = language[0:6:2] # 
-print(pto) # pto
+pto = language[0:6:2] # берем 0, 2 и 4 индексы
+print(pto) # Pto
 
-# Escape sequence
-print('I hope every one enjoying the python challenge.\nDo you ?') # line break
-print('Days\tTopics\tExercises')
-print('Day 1\t3\t5')
-print('Day 2\t3\t5')
-print('Day 3\t3\t5')
-print('Day 4\t3\t5')
-print('This is a back slash  symbol (\\)') # To write a back slash
-print('In every programming language it starts with \"Hello, World!\"')
+# Экранирование последовательностей
+print('Я надеюсь, что каждый получает удовольствие от Python Challenge.\nА вы получаете?') # Перенос строки
+print('Дни\tТемы\tУпражнения') # Добавление табуляции или 4-х пробелов
+print('День 1\t3\t5')
+print('День 2\t3\t5')
+print('День 3\t3\t5')
+print('День 4\t3\t5')
+print('Это символ обратной косой черты (\\)') # Запись обратной косой черты
+print('Изучения языков программирования принято начинать с фразы \"Hello, World!\"') # Запись двойных кавычек внутри одинарных
 
-## String Methods
-# capitalize(): Converts the first character the string to Capital Letter
+## Методы строк
 
-challenge = 'thirty days of python'
-print(challenge.capitalize()) # 'Thirty days of python'
+# capitalize(): Преобразует первый символ строки в заглавную букву.
 
-# count(): returns occurrences of substring in string, count(substring, start=.., end=..)
+challenge = 'тридцать дней Python'
+print(challenge.capitalize()) # 'Тридцать дней Python'
 
-challenge = 'thirty days of python'
-print(challenge.count('y')) # 3
-print(challenge.count('y', 7, 14)) # 1
-print(challenge.count('th')) # 2`
+# count(): Возвращает количество вхождений подстроки в строку. Метод имеет следующий синтаксис: count(substring, start=..., end=...). Аргумент start принимает начальный индекс, а аргумент end принимает последний индекс, где будет вестись поиск подстроки.
 
-# endswith(): Checks if a string ends with a specified ending
+challenge = 'тридцать дней python'
+print(challenge.count('т')) # 2
+print(challenge.count('д', 7, 14)) # 1,
+print(challenge.count('th')) # 1
 
-challenge = 'thirty days of python'
+# endswith(): Проверяет, заканчивается ли строка указанным набором символов
+
+challenge = 'тридцать дней python'
 print(challenge.endswith('on'))   # True
 print(challenge.endswith('tion')) # False
 
-# expandtabs(): Replaces tab character with spaces, default tab size is 8. It takes tab size argument
+# expandtabs(): Заменяет символ табуляции пробелами. Размер табуляции по умолчанию равен 8, а данный метод принимает аргумент и изменяет размер табуляции.
 
-challenge = 'thirty\tdays\tof\tpython'
-print(challenge.expandtabs())   # 'thirty  days    of      python'
-print(challenge.expandtabs(10)) # 'thirty    days      of        python'
+challenge = 'тридцать\tдней\tpython'
+print(challenge.expandtabs())   # 'тридцать  дней      python'
+print(challenge.expandtabs(10)) # 'тридцать    дней        python'
 
-# find(): Returns the index of first occurrence of substring
+# find(): Возвращает индекс первого вхождения подстроки, если подстрока не найдена, возвращает -1.
 
-challenge = 'thirty days of python'
-print(challenge.find('y'))  # 5
-print(challenge.find('th')) # 0
+challenge = 'традцать дней python'
+print(challenge.find('y'))  # 15
+print(challenge.find('th')) # 16
 
-# format()	formats string into nicer output    
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-job = 'teacher'
-country = 'Finland'
-sentence = 'I am {} {}. I am a {}. I live in {}.'.format(first_name, last_name, job, country)
-print(sentence) # I am Asabeneh Yetayeh. I am a teacher. I live in Finland.
+# rfind(): Возвращает индекс последнего вхождения подстроки, если подстрока не найдена, возвращает -1.
+
+challenge = 'тридцать дней python вместе'
+print(challenge.rfind('т'))  # 6
+print(challenge.rfind('е')) # 26
+
+# format(): форматирует строку для более красивого вывода.
+
+first_name = 'Питер'
+last_name = 'Паркер'
+age = 250
+job = 'учитель'
+country = 'Италия'
+sentence = 'Меня зовут {} {}. Мне {} лет. Я {}. Моя любимая страна {}.'.format(first_name, last_name, age, job, country)
+print(sentence) # Меня зовут Питер Паркер. Мне 250 лет. Я учитель. Моя любимая страна Италия.
 
 radius = 10
 pi = 3.14
-area = pi # radius ## 2
-result = 'The area of circle with {} is {}'.format(str(radius), str(area))
-print(result) # The area of circle with 10 is 314.0
+area = pi * radius ** 2
+result = 'Окружность круга с радиусом {} равна {}'.format(str(radius), str(area))
+print(result) # Окружность круга с радиусом 10 равна 314
 
-# index(): Returns the index of substring
+# index(): Возвращает наименьший индекс подстроки. В качестве дополнительных аргументов могут выступать начальный и конечный индексы. Если подстрока не найдена, возвращает ошибку valueError.
+
+challenge = 'тридцать дней python'
+sub_string = 'ца'
+print(challenge.index(sub_string))  # 4
+print(challenge.index(sub_string, 5)) # error
+
+# rindex(): Возвращает наибольший индекс подстроки. В качестве дополнительных аргументов также могут выступать начальный и конечный индексы.
+
 challenge = 'thirty days of python'
-print(challenge.find('y'))  # 5
-print(challenge.find('th')) # 0
+sub_string = 'da'
+print(challenge.rindex(sub_string))  # 8
+print(challenge.rindex(sub_string, 9)) # error
 
-# isalnum(): Checks alphanumeric character
+# isalnum(): Проверяет, содержит ли строка только буквенно-цифровые символы.
 
-challenge = 'ThirtyDaysPython'
+challenge = 'ТридцатьДнейPython'
 print(challenge.isalnum()) # True
 
-challenge = '30DaysPython'
+challenge = '30ДнейPython'
 print(challenge.isalnum()) # True
 
-challenge = 'thirty days of python'
+challenge = 'тридцать дней python'
+print(challenge.isalnum()) # False, пробел не является буквенно-цифровым символом
+
+challenge = 'тридцать дней python 2019'
 print(challenge.isalnum()) # False
 
-challenge = 'thirty days of python 2019'
-print(challenge.isalnum()) # False
+# isalpha(): Проверяет, состоит ли строка только из буквенных символов.
 
-# isalpha(): Checks if all characters are alphabets
-
-challenge = 'thirty days of python'
+challenge = 'тридцать дней python'
+print(challenge.isalpha()) # False, пробел снова не включается
+challenge = 'ТридцатьДнейPython'
 print(challenge.isalpha()) # True
 num = '123'
 print(num.isalpha())      # False
 
-# isdecimal(): Checks Decimal Characters
+# isdecimal(): Проверяет, являются ли все символы в строке из десятичных цифр (от 0 до 9).
 
-challenge = 'thirty days of python'
-print(challenge.find('y'))  # 5
-print(challenge.find('th')) # 0
+challenge = 'тридцать дней python'
+print(challenge.isdecimal())  # False
+challenge = '123'
+print(challenge.isdecimal())  # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # False
+challenge = '12 3'
+print(challenge.isdecimal())  # False, пробел не допускается
 
-# isdigit(): Checks Digit Characters
+# isdigit(): Проверяет, являются ли все символы в строке числами (от 0 до 9 и некоторми другми символами Юникода, представляющими числа).
 
-challenge = 'Thirty'
+challenge = 'Тридцать'
 print(challenge.isdigit()) # False
 challenge = '30'
-print(challenge.digit())   # True
+print(challenge.isdigit())   # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # True
 
-# isdecimal():Checks decimal characters
+# isnumeric(): Проверяет, являются ли все символы в строке числами или связаными с числами символами(аналогично isdigit(), но принимает больше символов, например, ½).
 
 num = '10'
-print(num.isdecimal()) # True
+print(num.isnumeric()) # True
+num = '\u00BD' # ½
+print(num.isnumeric()) # True
 num = '10.5'
-print(num.isdecimal()) # False
+print(num.isnumeric()) # False
 
-
-# isidentifier():Checks for valid identifier means it check if a string is a valid variable name
+# isidentifier(): Проверяет, является ли строка допустимым именем переменной.
 
 challenge = '30DaysOfPython'
-print(challenge.isidentifier()) # False, because it starts with a number
+print(challenge.isidentifier()) # False, потому что начинается с числа
 challenge = 'thirty_days_of_python'
 print(challenge.isidentifier()) # True
 
+# islower(): Проверяет, являются ли все буквы в строке __строчными__.
 
-# islower():Checks if all alphabets in a string are lowercase
-
-challenge = 'thirty days of python'
+challenge = 'тридцать дней python'
 print(challenge.islower()) # True
-challenge = 'Thirty days of python'
+challenge = 'Тридцать дней python'
 print(challenge.islower()) # False
 
-# isupper(): returns if all characters are uppercase characters
+# isupper(): Проверяет, являются ли все буквы в строке __заглавными__.
 
-challenge = 'thirty days of python'
+challenge = 'тридцать дней python'
 print(challenge.isupper()) #  False
-challenge = 'THIRTY DAYS OF PYTHON'
+challenge = 'ТРИДЦАТЬ ДНЕЙ PYTHON'
 print(challenge.isupper()) # True
 
-
-# isnumeric():Checks numeric characters
-
-num = '10'
-print(num.isnumeric())      # True
-print('ten'.isnumeric())    # False
-
-# join(): Returns a concatenated string
+# join(): Возвращает объединенную строку.
 
 web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
-result = '#, '.join(web_tech)
+result = ' '.join(web_tech)
+print(result) # 'HTML CSS JavaScript React'
+
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = '# '.join(web_tech)
 print(result) # 'HTML# CSS# JavaScript# React'
 
-# strip(): Removes both leading and trailing characters
+# strip(): Удаляет все указанные символы в начале и конце строки.
 
-challenge = ' thirty days of python '
-print(challenge.strip('y')) # 5
+challenge = 'тридцать дней pythoonnn'
+print(challenge.strip('noth')) # 'тридцать дней py'
+# replace(): Заменяет подстроку на заданную строку.
 
-# replace(): Replaces substring inside
+challenge = 'тридцать дней python'
+print(challenge.replace('python', 'удовольствия')) # 'стридцать дней удовольствия'
 
-challenge = 'thirty days of python'
-print(challenge.replace('python', 'coding')) # 'thirty days of coding'
+# split(): Разделяет строку, используя заданную строку или пробел в качестве разделителя.
 
-# split():Splits String from Left
+challenge = 'тридцать дней python'
+print(challenge.split()) # ['тридцать', 'дней', 'python']
+challenge = 'тридцать, дней, python'
+print(challenge.split(', ')) # ['тридцать', 'дней', 'python']
 
-challenge = 'thirty days of python'
-print(challenge.split()) # ['thirty', 'days', 'of', 'python']
+# title(): Возвращает строку с заглавными буквами в каждом слове.
 
-# title(): Returns a Title Cased String
+challenge = 'тридцать дней python'
+print(challenge.title()) # Тридцать Дней Python
 
-challenge = 'thirty days of python'
-print(challenge.title()) # Thirty Days Of Python
+# swapcase(): Преобразует все заглавные буквы в строчные и все строчные буквы в заглавные.
 
-# swapcase(): Checks if String Starts with the Specified String
-  
-challenge = 'thirty days of python'
-print(challenge.swapcase())   # THIRTY DAYS OF PYTHON
-challenge = 'Thirty Days Of Python'
-print(challenge.swapcase())  # tHIRTY dAYS oF pYTHON
+challenge = 'тридцать дней python'
+print(challenge.swapcase())   # ТРИДЦАТЬ ДНЕЙ PYTHON
+challenge = 'Тридцать Дней Python'
+print(challenge.swapcase())  # тРИДЦАТЬ дНЕЙ pYTHON
 
-# startswith(): Checks if String Starts with the Specified String
+# startswith(): Проверяет, начинается ли строка с указанной строки.
 
-challenge = 'thirty days of python'
-print(challenge.startswith('thirty')) # True
-challenge = '30 days of python'
+challenge = 'тридцать python'
+print(challenge.startswith('три')) # True
+
+challenge = '30 дней python'
 print(challenge.startswith('thirty')) # False
