ce59fc7aa053d7e841400c0a2732abb1659bc199
aashishdalmiapython <aashishdalmia10@gmail.com>
2023-07-09 13:06:16 +0530

    Added more programes

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -21,8 +21,8 @@ print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
 
 #Program to reverse 2 numbers
-a=50
-b=60
+a=int(input("Enter value of a:\t"))
+b=int(input("Enter Value of b:\t"))
 print("Value of a is:\t"+ str(a))
 print("Value of b is:\t"+ str(b))
 c =a+b
@@ -31,3 +31,27 @@ print("***************************************************")
 a=c-a
 print("Value of a is:\t"+str(a))
 print("Value of b is:\t"+ str(b))
+
+# Program to print table of any number
+
+number= int(input("Below is the table of :"))
+for i in range(1,11,1):
+    n = number * i
+    print(n)
+
+print("**********Factorial of a number:\t" + str(number))
+#Factorial of a numner
+for i in range (number-1,0,-1):
+    number=number*i
+print(number)
+
+# prime number.
+num = int(input("Enter the number for Prime check:\t"))
+z = 0
+for i in range(2, num):
+    if (num % i == 0):
+        print("Its not a Prime number")
+        z = 1
+        break
+print("This is Prime number")
+
