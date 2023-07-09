9231d44c028b0ea5b72ef45598d9199e92e3c155
aashishdalmiapython <aashishdalmia10@gmail.com>
2023-07-09 11:43:50 +0530

    Added code for reverting 2 numbers

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,3 +19,15 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+#Program to reverse 2 numbers
+a=50
+b=60
+print("Value of a is:\t"+ str(a))
+print("Value of b is:\t"+ str(b))
+c =a+b
+b=c-b
+print("***************************************************")
+a=c-a
+print("Value of a is:\t"+str(a))
+print("Value of b is:\t"+ str(b))
