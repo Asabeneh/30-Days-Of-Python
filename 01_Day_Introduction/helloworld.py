6f932477008bda182fdff70f4586136d6f5514d2
AdamHongBst <juerlain@gmail.com>
2023-08-04 21:21:38 +0700

    Show For the first time!

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,3 +19,4 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type((9.8, 3.14, 2.7)))    # Tuple
