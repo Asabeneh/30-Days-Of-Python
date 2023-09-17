8d7ce06d8f7995f011a401e191d9b6cd337f787c
Manuel Vergara <manuel@vergaracarmona.es>
2023-09-17 21:22:02 +0200

    Add exercise 3.2 of day 01
    Signed-off-by: Manuel Vergara <manuel@vergaracarmona.es>

---
+++ 01_Day_Introduction/euclidian_distance.py
@@ -0,0 +1,11 @@
+import math
+
+# Define the coordinates of the two points
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+# Calculate the Euclidean distance
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+# Print the result
+print("Euclidean distance:", distance)
