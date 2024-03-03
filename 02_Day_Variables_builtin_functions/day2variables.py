33e8519c9b9ed4459770d0a2f19e8367b2a56f34
Krystal <124251248+krys-t-ninja@users.noreply.github.com>
2024-03-03 15:46:49 +0000

    added file for Day 2 exercise 1
    Signed-off-by: GitHub <noreply@github.com>

---
+++ 02_Day_Variables_builtin_functions/day2variables.py
@@ -0,0 +1,74 @@
+# DAY 2 :30 DAYS OF PYTHON PROGRAMMING
+
+# Declare a first name variabke and assign a value to it
+
+
+from numpy import format_float_scientific
+from seaborn import FacetGrid
+from sympy import false
+
+
+first_name = 'Krystal'
+
+print(first_name)
+
+
+#Declare a last name variable and assign a value to it
+
+last_name = "Salinas"
+
+
+print(last_name)
+
+#Declare a full name variable and assign a value to it 
+
+full_name =(first_name , last_name)
+
+print(full_name)
+
+#Declare a country variable and assign a value to it
+country="America"
+
+print(country)
+
+
+
+#Declare a city variable and assign a value to it
+city = "San Antonio"
+
+print(city)
+
+#Declare an age  variable and assign a value to it
+
+age =36
+print(age)
+#Declare a year variable and assign a value to it
+year = 2024
+print(year)
+#Declare a variable is_married  and assign a value to it
+
+is_married = False
+
+print(is_married)
+#Declare a  variable is_true and assign a value to it
+
+is_true = len(first_name )<3
+
+print(is_true)
+#Declare a variable is_light_on  and assign a value to it
+is_light_on ="Yes, light is on" 
+
+
+
+print(is_light_on)
+
+
+        
+
+
+
+    
+
+#Declare a multiple variable on one line 
+
+print(first_name, is_light_on, year, last_name)
