cda8ab0b69892a12377d78a83a212699fe02756f
Krystal <124251248+krys-t-ninja@users.noreply.github.com>
2024-03-03 13:43:43 +0000

    small changes

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -14,8 +14,8 @@ print(3 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type('My name is Krystal'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
+print(type({'name':'Krystal'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
