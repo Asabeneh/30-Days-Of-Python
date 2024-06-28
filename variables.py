c438eba6950f4855d21e4b1e3a9c1ef7b0ddab9d
YaboDuulG <134439724+YaboDuulG@users.noreply.github.com>
2024-06-28 13:21:57 -0600

    Create variables.py
    Exercises for Day 2

---
+++ variables.py
@@ -0,0 +1,40 @@
+#Day 2: 30 Days of python programming
+#Exercise 1 
+first_name = "Randy"
+last_name = "Gomez"
+name = first_name + last_name
+country = "Mongolia"
+city = "Ulaanbaatar"
+age = "25"
+year = "2024"
+is_married, is_true, is_light_on = False, True, False
+
+#Exercise 2
+type(first_name)
+type(last_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+len(first_name)
+print("First Name is " + len(first_name) + "characters long, while Last Name is " + len(last_name) + " long")
+num_one, num_two = 5,4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = (radius**2)*3.14
+circum_of_circle = (radius*2)*3.14
+area_of_circleinpuit = (float(input("Please enter the radius of a circle: "))**2)*3.14
+UserFirstName = input("Please enter your First Name: ")
+UserLastName = input("Please enter your Last Name: ")
+UserCountry = input("Please enter your Country of origin: ")
+UserAge = input("Please enter your age in years: ")
+help('keywords')
