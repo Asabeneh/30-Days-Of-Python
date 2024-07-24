71fa1773b995e4b9f829016fccf374edf628d42b
Vasantan <vasa5431@gmail.com>
2024-07-24 14:38:48 +0800

    Day 1 exercise

---
+++ day_1/helloworld.py
@@ -0,0 +1,48 @@
+#Exercise: Level 2
+#1
+import sys
+print(sys.version)
+
+#2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3**4)
+print(3//4)
+
+#3
+print("Vasantan")
+print("Govinda Raju")
+print("Malaysia")
+print("I am enjoying 30 days of python")
+
+#4
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["vasantan","Python","Malaysia"]))
+print("Vasantan")
+print("Govinda Raju")
+print("Malaysia")
+
+#Exercise: Level 3
+
+#1
+print(type(5))
+print(type(5.1))
+print(type(4+5j))
+print(type(True))
+print(type("Hello"))
+print(type([1,4,5,"vasan"]))
+print(type((1,4,5)))
+print(type(set([1,4,5])))
+print(type({"key":3}))
+
+euclidian_distance = ((10-2)**2+(8-3)**2)**.5
+print(euclidian_distance)
+print(1)
