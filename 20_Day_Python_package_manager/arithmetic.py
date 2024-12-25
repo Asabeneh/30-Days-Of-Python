7b92fa6e9923593cd704aac0ca481c2ccfb818b6
Dharshini-457 <dharshinir619@gmail.com>
2024-12-25 22:11:15 +0530

    Arithmetic program commit

--- 20_Day_Python_package_manager/arithmetic.py
+++ 20_Day_Python_package_manager/arithmetic.py
@@ -1,7 +1,9 @@
 def add_numbers(*args):
     total = 0
     for num in args:
+        print(num)
         total += num
+        
     return total
 
 
@@ -23,3 +25,12 @@ def remainder(a, b):
 
 def power(a, b):
     return a ** b
+args=12345
+print(add_numbers(args))
+num1=10
+num2=20
+print(subtract(num1,num2))
+print(multiple(num1,num2))
+print(division(num1,num2))
+print(remainder(num1,num2))
+print(power(num1,num2))
