20f33ccbfc0f412e125b8c69bfdcbd9df3a661d4
Aren <95090911+rawbytedev@users.noreply.github.com>
2024-12-31 23:01:41 +0100

    uncomment

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -106,7 +106,7 @@ print(fruits)       # ['orange', 'mango', 'lemon']
 del fruits[1]     
 print(fruits)       # ['orange', 'lemon']
 del fruits
-#print(fruits)       # This should give: NameError: name 'fruits' is not defined
+print(fruits)       # This should give: NameError: name 'fruits' is not defined
 
 # clear
 fruits = ['banana', 'orange', 'mango', 'lemon']
