44a009b64663a5b88e2853f5122dfe7a866519f5
L. Abhilash <160405976+LAbhilashKumar@users.noreply.github.com>
2025-05-19 19:12:12 +0530

    Update indentation.py

--- indentation.py
+++ indentation.py
@@ -1,5 +1,5 @@
 """
-Whitespace at the beginning of the line is called 'indentation'. These whitespaces or the indentation are very important in python. In a Pythonprogram,
+Whitespace at the beginning of the line is called 'indentation'. These whitespaces or the indentation are very important in python. In a Python program,
 the leading whitespace including spaces and tabs at the beginning of the logical line determines the indentation level of that logic.
 
 * In most programming languages, indentation has no effect on progamming logic. It is used to align statements to make the code more readable.
@@ -7,7 +7,7 @@ However, in python, indentation is used to associate and group statements.
 
 All statements within the same block must have the same indentation level. Typically, one tab or 4 spaces are used per level.
 
-All statements inside a block should be at the same indentation level
+All statements inside a block should be at the same indentation level.
 
 
 Below is the correct syntax for nested conditions with proper indentation:
