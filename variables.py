49d60a47bfde925125888ef7e4f29e3638b8c70d
kk1v <kostjaivanov091@gmail.com>
2025-08-28 17:16:04 +0300

    Create variables.py
    day 2

---
+++ variables.py
@@ -0,0 +1,79 @@
+# 'Day 2: 30 Days of python programming'
+name = "Kostja"
+last_name = "Ivanov" 
+full_name = "Kostja Ivanov"
+country = "Estonia"
+city = 'Tallinn'
+age = 19
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+mylist = [ 1, 2, 3]
+
+print(type(name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(mylist))
+
+print(len("Kostja"))
+print(len("Ivanov"))
+
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+
+diff = num_two - num_one 
+print(diff)
+
+product = num_two * num_one
+print(product)
+
+delegate = num_one / num_two
+print(delegate)
+
+remainder = num_two % num_one
+print(remainder)
+
+exp = num_one ** num_two
+print(exp)
+
+floor_division = num_one // num_two 
+print(floor_division)
+
+
+r = 30
+area_of_circle = 3.14 * (r ** 2)
+print(area_of_circle)
+
+circum_of_circle = 2 * 3.14 * r
+print(circum_of_circle)
+
+
+r1 = float(input("Radius"))
+area_of_circle1 = 3.14 * (r1 ** 2)
+print(area_of_circle1)
+
+
+
+name1 = input("Name: ")
+last_name1 = input("Last name: ")
+country1 = input("Country: ")
+age1 = int(input("Age"))
+print(name1)
+print(last_name1)
+print(country1)
+print(age1)
+
+help(str)
+
+      
