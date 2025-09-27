131272d15284ee7b649d412f9300fe5568d8752b
Yug Shah <yugshah197@gmail.com>
2025-09-27 15:30:55 +0000

    September 27

---
+++ 03_Day_Operators/day-3myfile.py
@@ -0,0 +1,10 @@
+age = 25
+height = 5.9
+complex_num = 3 + 4j
+
+# base = input("Enter base: ")
+# height = input("Enter height: ")
+# area = 0.5 * float(base) * float(height)
+# print("The area of the triangle is " + str(area))
+
+side_a = input('Enter Side a: ')
