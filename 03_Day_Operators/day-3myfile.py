1761c855308dab5ccf01839519489943bfe29f60
Yug Shah <yugshah197@gmail.com>
2025-09-27 16:00:37 +0000

    septermer 27, 2024, 10:15 PM +0530

--- 03_Day_Operators/day-3myfile.py
+++ 03_Day_Operators/day-3myfile.py
@@ -1,10 +1,48 @@
-age = 25
-height = 5.9
-complex_num = 3 + 4j
+age = 25  #1
+height = 5.9 #2
+complex_num = 3 + 4j #3
+
+# print('==Stage 1: Variables==') #4
 
 # base = input("Enter base: ")
 # height = input("Enter height: ")
 # area = 0.5 * float(base) * float(height)
 # print("The area of the triangle is " + str(area))
 
-side_a = input('Enter Side a: ')
+# print('==Stage 2: Variables==') #5
+
+# side_a = input('Enter Side a: ')
+# side_b = input('Enter Side b: ')
+# side_c = input('Enter Side c: ')    
+# print('The perimeter of the triangle is: ' + str(float(side_a) + float(side_b) + float(side_c)))
+
+# print('==Stage 3: Variables==') #6
+
+# length = input('Enter length: ')
+# width = input('Enter width: ')
+# area = float(length) * float(width)
+# print('The area of the rectangle is: ' + str(area))/
+
+# print('==Stage 4: Variables==') #7
+
+# radius = input('Enter radius: ')
+# pi = 3.14
+# area = pi * float(radius) ** 2
+# circumference = 2 * pi * float(radius)
+# print('The area of the circle is: ' + str(area))
+# print('The circumference of the circle is: ' + str(circumference))
+
+# print('==Stage 5: Variables==') #8 
+
+# x1,x2,y1,y2 = 2,6,2,10
+# m = (y2 - y1) / (x2 - x1)
+# print('The slope of the line is: ' + str(m))    
+
+# print('==Stage 6: Variables==') #9
+# x = -3
+# y =x**2 + 6*x + 9
+# print('The value of y is: ' + str(y))   
+
+print('==Stage 7: Variables==') #10
+
+print(len('python') == len('dragon'))  # False
