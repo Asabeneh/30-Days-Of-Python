da4431d7f7644163defba86347b9e88933d796ca
Yug Shah <yugshah197@gmail.com>
2025-09-27 16:21:37 +0000

    septermber 27

--- 03_Day_Operators/day-3myfile.py
+++ 03_Day_Operators/day-3myfile.py
@@ -32,17 +32,55 @@ complex_num = 3 + 4j #3
 # print('The area of the circle is: ' + str(area))
 # print('The circumference of the circle is: ' + str(circumference))
 
-# print('==Stage 5: Variables==') #8 
+# print('==Stage 5: Variables==') #8 9
 
 # x1,x2,y1,y2 = 2,6,2,10
 # m = (y2 - y1) / (x2 - x1)
 # print('The slope of the line is: ' + str(m))    
 
-# print('==Stage 6: Variables==') #9
+# print('==Stage 6: Variables==') #11
 # x = -3
 # y =x**2 + 6*x + 9
 # print('The value of y is: ' + str(y))   
 
-print('==Stage 7: Variables==') #10
+# print('==Stage 7: Variables==') #12
 
-print(len('python') == len('dragon'))  # False
+# print(len('python') == len('dragon'))  # False 12
+
+# print('on' in 'python' and 'on' in 'dragon')  # True 13
+# print('jargon' in 'I hope this course is not full of jargon')  # True 14
+# print('on' not in 'python' and 'on' not in 'dragon')  # False 15
+# lenth = len('python')
+# print('length of python is ' , float(lenth))  # 16
+
+# print('==Stage 8: Variables==') #17
+# num = input('Enter a number: ')
+# if int(num) % 2 == 0:
+#     print(str(num) + ' is an even number')
+# else:
+#     print(str(num) + ' is an odd number')  
+
+
+# print('7//3 is equal to int(2.7) : ', 7//3 is int(2.7))  # True 18
+
+# print('"10" == 10 : ', '10' == 10)  # False 19
+
+# print('int("9.8") == 10 : ', int(float('9.8')) == 10)  # False 20
+
+# print('==Stage 9: Variables==') #21
+
+# hours = int(input('Enter hours: '))
+# rate = int(input('Enter rate per hour: '))
+# print('Your weekly earning is: ' + str(hours * rate))  
+
+# print('==Stage 10: Variables==') #22
+# years = int(input('Enter number of years you have lived: '))
+# print('You have lived for ' + str(years * 365 * 24 * 60 * 60) + ' seconds.')
+
+# print('==Stage 11: Variables==') #23
+print("""1 1 1 1 1
+2 1 2 4 8 
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125""")
+
