c7ce9b87009f971ec3d9bc27817c911532370944
Alexa Garcia <84157414+Alexagarcia5280@users.noreply.github.com>
2025-10-02 22:22:17 -0600

    Update variables_Alexa.py

--- 02_Day_Variables_builtin_functions/variables_Alexa.py
+++ 02_Day_Variables_builtin_functions/variables_Alexa.py
@@ -16,3 +16,32 @@ demographics = {
     'country': country,
     'city': city
 }
+
+
+print(first_name)
+print(len(first_name))
+
+print(last_name)
+print(len(last_name))
+
+print(full_name)
+print(len(full_name))
+
+print(country)
+print(len(country))
+
+print(city)
+print(len(city))
+
+print(age) #int has no len
+
+print(year) #int has no len
+
+print(is_married) #bool has no len
+
+print(is_true) #bool has no len
+
+print(is_light_on) #bool has no len
+
+print(demographics)
+print(len(demographics))
