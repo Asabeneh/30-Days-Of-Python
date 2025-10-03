184f5e4e95fef3c6267610067b76a19e67961744
Alexa Garcia <84157414+Alexagarcia5280@users.noreply.github.com>
2025-10-02 21:19:33 -0600

    Update variables_Alexa.py

--- 02_Day_Variables_builtin_functions/variables_Alexa.py
+++ 02_Day_Variables_builtin_functions/variables_Alexa.py
@@ -1,3 +1,10 @@
 #Day 2: 30 days of python programming 
 
-first_name 
+first_name = 'Alexa'
+last_name = 'Garcia'
+full_name = first_name + " " + last_name
+
+country = 'United States'
+city = 'Salt Lake City'
+age = 37
+year = 2025
