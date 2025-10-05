2d93423fd9183b4f5dab046236244c849c5287d0
Alexa Garcia <84157414+developedbyAlexa@users.noreply.github.com>
2025-10-05 10:17:10 -0600

    Update data_types_Alexa.py

--- 01_Day_Introduction/data_types_Alexa.py
+++ 01_Day_Introduction/data_types_Alexa.py
@@ -7,8 +7,12 @@
 Your name
 Your family name
 Your country
-Exercise: Level 2
+
+
+#Exercise: Level 2
 Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it.
-Exercise: Level 3
+
+  
+#Exercise: Level 3
 Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
 Find an Euclidian distance between (2, 3) and (10, 8)
