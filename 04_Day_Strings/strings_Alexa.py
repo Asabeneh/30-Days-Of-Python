6a9ba0114ac1de57a8fc05d1e5daf441530e4ddf
Alexa Garcia <84157414+developedbyAlexa@users.noreply.github.com>
2025-10-07 17:42:14 -0600

    Update strings_Alexa.py

--- 04_Day_Strings/strings_Alexa.py
+++ 04_Day_Strings/strings_Alexa.py
@@ -1,5 +1,5 @@
 #Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
-Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
 Declare a variable named company and assign it to an initial value "Coding For All".
 Print the variable company using print().
 Print the length of the company string using len() method and print().
