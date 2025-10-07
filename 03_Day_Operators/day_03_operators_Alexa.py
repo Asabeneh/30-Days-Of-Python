fb230db1514d39859ab65c918302ca381f913275
Alexa Garcia <84157414+developedbyAlexa@users.noreply.github.com>
2025-10-07 08:35:29 -0600

    Update day_03_operators_Alexa.py

--- 03_Day_Operators/day_03_operators_Alexa.py
+++ 03_Day_Operators/day_03_operators_Alexa.py
@@ -1,1 +1,23 @@
+#Exercises - Day 3
 
+#Declare your age as integer variable
+age = 35
+print(age)
+
+#Declare your height as a float variable
+height_in_inches = 63.0
+print(height_in_inches
+
+#Declare a variable that store a complex number
+complex = 3 + 4j
+print(complex)
+      
+#Write a script that prompts the user to enter base and height of the triangle 
+      #and calculate an area of this triangle (area = 0.5 x b x h).
+
+user_input_base = input("enter base of triangle: " + ")
+user_input_height = input("enter height of triangle: " + ")
+# Calculating area of a circle
+area = 0.5
+area_of_triangle = 3.14 * area ** 2
+print('Area of a triangle:', area_of_triangle)
