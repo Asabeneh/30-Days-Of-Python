c8c6a9435947ddeaf69066c720092c21c9947104
Reece Brooker <reecebrooker98@hotmail.co.uk>
2025-12-15 02:52:17 +0000

    Create variables.py

---
+++ 02_Day_Variables_builtin_functions/day_2/variables.py
@@ -0,0 +1,12 @@
+# Day 2: 30 Days of python programming
+first_name = 'Reece'
+last_name = 'Brooker'
+full_name = 'Reece Brooker'
+country = 'England'
+city = 'Peterborough'
+age = 27
+year = 1998
+is_married = married
+is_true = true
+is_light_on = true
+print(first_name)
