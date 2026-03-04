ab853fe1dca1767ef1cd5068028b38fed3bd5a6f
Shaun <shaunmane.sm@gmail.com>
2026-03-04 16:08:16 +0200

    level 2 #1

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -109,11 +109,28 @@ full_stack.insert(6, 'SQL')
 ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
 # - Sort the list and find the min and max age
+ages.sort()
+min_age = min(ages)
+max_age = max(ages)
+
 # - Add the min age and the max age again to the list
+add_age = [min(ages)] + [max(ages)]
+ages.extend(add_age)
+
 # - Find the median age (one middle item or two middle items divided by two)
+ages.sort()
+mean = (ages[4] + ages[5]) / 2
+
 # - Find the average age (sum of all items divided by their number )
+ave = sum(ages) / len(ages)
+
 # - Find the range of the ages (max minus min)
-# - Compare the value of (min - average) and (max - average), use _abs()_ method
+ages_range = max(ages) - min(ages)
+
+# - Compare the value of (min - average) and (max - average), use abs() method
+ave = sum(ages) / len(ages)
+ave_min = abs(min(ages) - ave)
+ave_max = abs(max(ages) - ave)
 
 # 1. Find the middle country(ies) in the [countries list](https://github.com/Asabeneh/30-Days-Of-Python/tree/master/data/countries.py)
 # 2. Divide the countries list into two equal lists if it is even if not one more country for the first half.
