bc0ceef48f7ac89fe89ea2decab40d123e9f882a
Shaun <shaunmane.sm@gmail.com>
2026-03-06 15:24:51 +0200

    completed level 2

--- 06_Day_Tuples/day_6.py
+++ 06_Day_Tuples/day_6.py
@@ -25,16 +25,32 @@ family_members = siblings + ("Rick", "Beth")
 # ---------------------
 
 # 1. Unpack siblings and parents from family_members
-# 1. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
-# 1. Change the about food_stuff_tp  tuple to a food_stuff_lt list
-# 1. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
-# 1. Slice out the first three items and the last three items from food_stuff_lt list
-# 1. Delete the food_stuff_tp tuple completely
-# 1. Check if an item exists in  tuple:
+family_members = family_members[:3], family_members[-2:]
+siblings, parents = family_members
 
+# 2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+fruits = ("apple", "banana", "peach", "tomato")
+vegetables = ("carrot", "bell pepper", "potato")
+animal_products = ("cheese", "beef mince", "honey")
+
+food_stuff_tp = fruits + vegetables + animal_products
+
+# 3. Change the about food_stuff_tp  tuple to a food_stuff_lt list
+food_stuff_lt = list(food_stuff_tp)
+
+# 4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+food_stuff_tp = food_stuff_tp[:4] + food_stuff_tp[-4:]
+
+# 5. Slice out the first three items and the last three items from food_stuff_lt list
+food_stuff_lt = food_stuff_lt[3:-3]
+
+# 6. Delete the food_stuff_tp tuple completely
+del food_stuff_tp
+
+# 7. Check if an item exists in  tuple:
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 # - Check if 'Estonia' is a nordic country
-# - Check if 'Iceland' is a nordic country
+estonia_check = "Estonia" in nordic_countries
 
-#   ```py
-#   nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
-#   ```
+# - Check if 'Iceland' is a nordic country
+iceland_check = "Iceland" in nordic_countries
