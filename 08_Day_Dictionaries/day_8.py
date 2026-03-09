3ccfb51b744e20db2d60f9ab7af4793e3d699777
Shaun <shaunmane.sm@gmail.com>
2026-03-09 17:15:44 +0200

    created day-8

---
+++ 08_Day_Dictionaries/day_8.py
@@ -0,0 +1,15 @@
+# ─────────────────────────────────────────────
+## 💻 Exercises: Day 8
+# ─────────────────────────────────────────────
+
+# 1. Create  an empty dictionary called dog
+# 2. Add name, color, breed, legs, age to the dog dictionary
+# 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
+# 4. Get the length of the student dictionary
+# 5. Get the value of skills and check the data type, it should be a list
+# 6. Modify the skills values by adding one or two skills
+# 7. Get the dictionary keys as a list
+# 8. Get the dictionary values as a list
+# 9. Change the dictionary to a list of tuples using _items()_ method
+# 10. Delete one of the items in the dictionary
+# 11. Delete one of the dictionaries
