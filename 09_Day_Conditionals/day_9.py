9ea2f7884ec64f4e55747eee7f786dd61481506d
Shaun <shaunmane.sm@gmail.com>
2026-03-11 18:11:29 +0200

    exercises

--- 09_Day_Conditionals/day_9.py
+++ 09_Day_Conditionals/day_9.py
@@ -6,7 +6,6 @@
 ### Exercises: Level 1
 # ─────────────────────────────────────────────
 # 1. Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
-
 #     ```sh
 #     Enter your age: 30
 #     You are old enough to learn to drive.
@@ -16,14 +15,12 @@
 #     ```
 
 # 2. Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. Output:
-
 #     ```sh
 #     Enter your age: 30
 #     You are 5 years older than me.
 #     ```
 
 # 3. Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b. Output:
-
 # ```sh
 # Enter number one: 4
 # Enter number two: 3
@@ -34,7 +31,6 @@
 ### Exercises: Level 2
 # ─────────────────────────────────────────────
 #    1. Write a code which gives grade to students according to theirs scores:
-
 #     ```sh
 #     90-100, A
 #     80-89, B
@@ -48,13 +44,13 @@
 #     December, January or February, the season is Winter.
 #     March, April or May, the season is Spring
 #     June, July or August, the season is Summer
-#    3. The following list contains some fruits:
 
+#    3. The following list contains some fruits:
 #     ```sh
 #     fruits = ['banana', 'orange', 'mango', 'lemon']
 #     ```
-
 #     If a fruit doesn't exist in the list add the fruit to the list and print the modified list. If the fruit exists print('That fruit already exist in the list')
+
 
 # ─────────────────────────────────────────────
 ### Exercises: Level 3
