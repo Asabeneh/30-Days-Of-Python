b40e532203af0245c056ac8d263bbe3e0a1f7b3a
Abhimanyubhadauriya <166346229+Abhimanyubhadauriya@users.noreply.github.com>
2026-03-26 00:37:23 +0530

    Fix type comment for set in helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -22,4 +22,4 @@ print(type(1 + 3j))              # Complex
 print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
 print(type({'name': 'Asabeneh'}))  # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Tuple
+print(type({9.8, 3.14, 2.7}))    # Set
