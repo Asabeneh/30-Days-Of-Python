a46f295b8269b16cc0cd723521e555316451187a
Shaun <shaunmane.sm@gmail.com>
2026-04-14 21:39:07 +0200

    completed 9 to 12

--- 11_Day_Functions/level1.py
+++ 11_Day_Functions/level1.py
@@ -115,9 +115,9 @@ def add_item(mylist, item):
 
 # 12. Declare a function named remove_item. It takes a list and an item parameters. It returns a list with the item removed from it.
 # food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk']
-# print(remove_item(food_stuff, 'Mango'))  # ['Potato', 'Tomato', 'Milk'];
+# print(remove_item(food_stuff, 'Mango'))   # ['Potato', 'Tomato', 'Milk'];
 # numbers = [2, 3, 7, 9]
-# print(remove_item(numbers, 3))  # [2, 7, 9]
+# print(remove_item(numbers, 3))            # [2, 7, 9]
 def remove_item(mylist, item):
     mylist.remove(item)
     return mylist
