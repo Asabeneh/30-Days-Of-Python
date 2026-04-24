d9fc6d6040d177aaf9ed10c5e6174cd14f2a9b0d
Shaun <shaunmane.sm@gmail.com>
2026-04-24 18:11:51 +0200

    began level3

---
+++ 11_Day_Functions/day_11_level3.py
@@ -0,0 +1,30 @@
+# ─────────────────────────────────────────────
+## 💻 Exercises: Day 11
+# ─────────────────────────────────────────────
+
+# ─────────────────────────────────────────────
+### Exercises: Level 3
+# ─────────────────────────────────────────────
+
+# 1. Write a function called is_prime, which checks if a number is prime.
+def is_prime(number):
+    if type(number) == int:
+        if number > 1:
+            if number == 2:
+                return f"{number} is a Prime"
+            elif (number % 2) and (number % 3) and (number % 5 ) > 0:
+                return f"{number} is a Prime"
+            else:
+                return f"{number} is not a Prime"
+        else:
+            return f"Only positive numbers can be Prime"
+    else:
+        return f"{type(number)} is not a Prime"
+print(is_prime(104728))      
+
+# 2. Write a functions which checks if all items are unique in the list.
+# 3. Write a function which checks if all the items of the list are of the same data type.
+# 4. Write a function which check if provided variable is a valid python variable
+# 5. Go to the data folder and access the countries-data.py file.
+# - Create a function called the most_spoken_languages in the world. It should return 10 or 20 most spoken languages in the world in descending order
+# - Create a function called the most_populated_countries. It should return 10 or 20 most populated countries in descending order.
