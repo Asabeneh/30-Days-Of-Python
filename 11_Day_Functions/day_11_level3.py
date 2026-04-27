9c98a34e760dd76176316e3423d7a007ed64a249
Shaun <shaunmane.sm@gmail.com>
2026-04-27 18:33:32 +0200

    L3 - number 3

--- 11_Day_Functions/day_11_level3.py
+++ 11_Day_Functions/day_11_level3.py
@@ -36,7 +36,17 @@ def unique(nums):
     return f'All items are unique'
 
 # 3. Write a function which checks if all the items of the list are of the same data type.
+def all_unique_types(nums):
+    seen = type(nums[0])
+    for item in nums:
+        if type(item) != seen:
+            return False
+    return True
+print(all_unique_types([2, 4, 6, 8, 10]))
+
 # 4. Write a function which check if provided variable is a valid python variable
+
+
 # 5. Go to the data folder and access the countries-data.py file.
 # - Create a function called the most_spoken_languages in the world. It should return 10 or 20 most spoken languages in the world in descending order
 # - Create a function called the most_populated_countries. It should return 10 or 20 most populated countries in descending order.
