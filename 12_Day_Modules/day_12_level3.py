a65ced71f12aec61413a1473d3afd19488dae734
Shaun <shaunmane.sm@gmail.com>
2026-05-03 22:37:56 +0200

    completed L3 & day12

--- 12_Day_Modules/day_12_level3.py
+++ 12_Day_Modules/day_12_level3.py
@@ -6,7 +6,6 @@
 ### Exercises: Level 3
 # ─────────────────────────────────────────────
 # 1. Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
-import string
 import random
 
 def shuffle_list(lst: list) -> list:
@@ -15,6 +14,9 @@ def shuffle_list(lst: list) -> list:
         shuffled_list.append(random.choice(lst))
     return shuffled_list
 
-#print(shuffle_list([1,2,3,4,5,6]))
 # 2. Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
 def array_of_seven() -> set[int]:
+    array = set()
+    while len(array) < 7:
+        array.add(random.randint(0,9))
+    return array
