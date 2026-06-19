bfeb28019c1e8f1336a267e5f6269bec15a00697
Jan Blahout <47158701+JanBlahout@users.noreply.github.com>
2026-06-19 17:01:02 +0200

    01_Day_Introduction
    Fix wrong comment, add Set and Tuple example

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -23,3 +23,5 @@ print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
 print(type({'name': 'Asabeneh'}))  # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type(("One","Two",3)))      # Tuple
