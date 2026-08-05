5f6266e104711e229a9af282286ae4c6a39ebbaf
Bhratesh <bhrateshd@gmail.com>
2026-08-05 23:49:11 +0530

    Day-2 Practice

---
+++ 02_Day_2/variables.py
@@ -0,0 +1,13 @@
+# Day 2: 30 Days of Python Programming
+
+first_name = "Bhratesh"
+Last_name = "Dhangar"
+Full_name = "Bhratesh Dhangar"
+Country_name = "India"
+City_name = "Pune"
+Age = 27
+Year = 2024
+Is_married = False
+Is_true = True
+Is_light_on = True
+First_name, Last_name, Age = "Bhratesh", "Dhangar", 27
